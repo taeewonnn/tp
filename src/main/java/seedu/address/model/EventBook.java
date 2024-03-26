263b09dec6cf68872cb770670218e0006b8dc993
Apzure <block4556@gmail.com>
2024-03-26 23:36:16 +0800

    Modify Edit Command to auto edit the same person in all events

--- src/main/java/seedu/address/model/EventBook.java
+++ src/main/java/seedu/address/model/EventBook.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import static java.util.Objects.requireNonNull;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javafx.collections.ObservableList;
@@ -125,7 +126,7 @@ public class EventBook implements ReadOnlyEventBook {
      */
     public void selectEvent(Event event) {
         selectedEvent = event;
-        personsOfSelectedEvent.setPersons(event.getPersonList());
+        updatePersonListOfSelectedEvent();
     }
 
     /**
@@ -133,7 +134,7 @@ public class EventBook implements ReadOnlyEventBook {
      */
     public void deselectEvent() {
         selectedEvent = null;
-        personsOfSelectedEvent.setPersons(new ArrayList<>());
+        updatePersonListOfSelectedEvent();
     }
 
     /**
@@ -158,7 +159,7 @@ public class EventBook implements ReadOnlyEventBook {
     public void addPersonToSelectedEvent(Person person) {
         if (isAnEventSelected()) {
             selectedEvent.addPerson(person);
-            personsOfSelectedEvent.add(person);
+            updatePersonListOfSelectedEvent();
         }
     }
 
@@ -171,7 +172,32 @@ public class EventBook implements ReadOnlyEventBook {
     public void deletePersonFromSelectedEvent(Person person) {
         if (isAnEventSelected()) {
             selectedEvent.deletePerson(person);
-            personsOfSelectedEvent.remove(person);
+            updatePersonListOfSelectedEvent();
+        }
+    }
+
+    /**
+     * Replaces an existing person in all events.
+     * If a person matching 'target' is found in an event, they are replaced with 'editedPerson'.
+     *
+     * @param target The person to be replaced.
+     * @param editedPerson The replacement person.
+     */
+    public void setPerson(Person target, Person editedPerson) {
+        for (Iterator<Event> it = events.iterator(); it.hasNext(); ) {
+            Event event = it.next();
+            if (event.hasPerson(target)) {
+                event.setPerson(target, editedPerson);
+            }
+        }
+        updatePersonListOfSelectedEvent();
+    }
+
+    private void updatePersonListOfSelectedEvent() {
+        if (selectedEvent != null) {
+            personsOfSelectedEvent.setPersons(selectedEvent.getPersonList());
+        } else {
+            personsOfSelectedEvent.setPersons(new ArrayList<>());
         }
     }
 
