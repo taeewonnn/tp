74bd7d4e71fa6a55742296aecad469b15f8e5fd9
taeewonnn <htw10192000@gmail.com>
2024-04-02 08:46:35 +0800

    Improve checkstyle

--- src/test/java/seedu/address/model/person/TagContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/TagContainsKeywordsPredicateTest.java
@@ -1,15 +1,16 @@
 package seedu.address.model.person;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.model.tag.Tag;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.tag.Tag;
 
 public class TagContainsKeywordsPredicateTest {
 
