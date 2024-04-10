cbbf7e555988b10d95ddb3160e3e7a130efd092a
nusliuyifan <tonyyifanliu@gmail.com>
2024-04-10 17:34:52 +0800

    delete "change it back" in the error message of delete event and delete person command

--- src/main/java/seedu/address/logic/commands/DeleteEventCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteEventCommand.java
@@ -22,7 +22,7 @@ public class DeleteEventCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the event identified by the index number used in the displayed event list.\n"
-            + "Parameters: INDEX (must be a positive integer and have a corresponding event)\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_EVENT_SUCCESS = "Deleted Event: %1$s";
